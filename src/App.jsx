b5eb6a128d575789ed6a024f0c4c6d138f1f761b
bushido <nakarasido5@gmail.com>
2024-02-06 22:11:17 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Header from "./component/Header.jsx";
+import Footer from "./component/Footer.jsx";
+
+function App() {
+  return (
+    <div>
+        <Header/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default App
