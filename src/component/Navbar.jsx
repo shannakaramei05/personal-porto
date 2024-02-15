f14ad799a166b1a8baab0407aef7c56bcce10f0e
bushido <nakarasido5@gmail.com>
2024-02-15 21:27:29 +0700

    add humburger for small screen

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,6 @@
 import { MdDarkMode } from "react-icons/md";
 import NavLink from "../route/NavLink.jsx";
-import logo from "../assets/react.svg"
+import logo from "../assets/react.svg";
 
 const Navbar = () => {
     return (
@@ -8,18 +8,27 @@ const Navbar = () => {
             <div className="container mx-auto px-6 lg:px-8 py-4 text-white">
                 <div className="flex justify-between items-center">
                     {/* Logo */}
-                    <img className="" src={logo} alt="logo" />
+                    <img className="h-8" src={logo} alt="logo" />
 
                     {/* Navigation Links */}
-                    <div className="flex-grow flex justify-center">
+                    <div className="hidden lg:flex flex-grow justify-center">
                         <NavLink href="#">Services</NavLink>
                         <NavLink href="#">Portfolio</NavLink>
                         <NavLink href="#">Experience</NavLink>
                         <NavLink href="#">Blog</NavLink>
                     </div>
 
+                    {/* Hamburger Menu for Small Screens */}
+                    <div className="lg:hidden">
+                        <button className="flex items-center text-white focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+
                     {/* Right Side Content */}
-                    <div className="flex items-center">
+                    <div className="hidden lg:flex items-center">
                         {/* Dark Mode Button */}
                         <div className="mr-6">
                             <MdDarkMode />
